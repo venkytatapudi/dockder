d69559abe441e988ff05faf7594c31f4bc775205
venkat tatapudi <96809250+venkytatapudi@users.noreply.github.com>
2022-02-19 00:02:40 +0530

    Update server.js

--- nodejs-docker/server.js
+++ nodejs-docker/server.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('jaffer langa!');
 });
 server.listen(8080);
